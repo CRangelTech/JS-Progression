f2da3f62d62b3716bf40c50b4da41c75e1d5a5b2
Carlos Cruz Rangel <carlosrangel.tech@gmail.com>
2024-12-09 19:12:52 -0600

    Create 2-tipos-de-datos.js

---
+++ ManejoDeVariables/2-tipos-de-datos.js
@@ -0,0 +1,41 @@
+
+/* The code `let miEntero = 10;` is declaring a variable `miEntero` and assigning it the value of type numeber `10`. */
+let miEntero = 10;
+console.log(miEntero);
+
+/* The code `miEntero= "Hola"; console.log(miEntero);` is reassigning the variable `miEntero` with the
+string value `"Hola"` and then logging the updated value of `miEntero` to the console. */
+miEntero= "Hola";
+console.log(miEntero);
+
+/* The code `let miFlotatante = 7.0; console.log(miFlotatante);` is declaring a variable `miFlotatante`
+and assigning it the floating-point number value `7.0`. Then it logs the value of `miFlotatante` to
+the console, which will output integer number `7`. */
+let miFlotatante = 7.0;
+console.log(miFlotatante);
+
+/* The code `let miCadena = 'Hola'; console.log(miCadena);` is declaring a variable `miCadena` and
+assigning it the string value `'Hola'`. Then it logs the value of `miCadena` to the console, which
+will output `'Hola'`. */
+let miCadena = 'Hola';
+console.log(miCadena);
+
+/* The code `let miBoolean = true; console.log(miBoolean);` is declaring a variable `miBoolean` and
+assigning it the boolean value `true`. Then it logs the value of `miBoolean` to the console, which
+will output `true`. */
+let miBoolean = true;
+console.log(miBoolean);
+
+/* The code `let miNull = null; console.log(miNull);` is declaring a variable `miNull` and assigning it
+the value `null`, which represents the intentional absence of any object value. Then it logs the
+value of `miNull` to the console, which will output `null`. */
+let miNull = null;
+console.log(miNull);
+
+/* The code `let miUndefined = undefined; console.log(miUndefined);` is declaring a variable
+`miUndefined` and assigning it the value `undefined`. Then it logs the value of `miUndefined` to the
+console, which will output `undefined`. This is a common practice to initialize a variable without
+assigning a specific value to it, indicating that it has not been defined yet or does not have a
+meaningful value at that point in the code execution. */
+let miUndefined = undefined;
+console.log(miUndefined);
