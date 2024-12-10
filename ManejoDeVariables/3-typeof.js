50308d8c4867ecc46460880004c35eaf2117d3e6
Carlos Cruz Rangel <carlosrangel.tech@gmail.com>
2024-12-09 22:05:04 -0600

    Create 3-typeof.js

---
+++ ManejoDeVariables/3-typeof.js
@@ -0,0 +1,82 @@
+
+/* The code `let miEntero = 10;` is declaring a variable `miEntero` and assigning it the value of type numeber `10`. */
+let miEntero = 10;
+console.log(miEntero);
+
+/* `console.log(typeof miEntero);` is logging the data type of the variable `miEntero` to the console.
+The `typeof` operator in JavaScript is used to determine the type of a variable or value. In this
+case, it will output `number` to the console, indicating that the variable `miEntero` is of type
+number. */
+console.log(typeof miEntero);
+
+/* The code `miEntero= "Hola"; console.log(miEntero);` is reassigning the variable `miEntero` with the
+string value `"Hola"` and then logging the updated value of `miEntero` to the console. */
+miEntero= "Hola";
+console.log(miEntero);
+
+/* `console.log(typeof miEntero);` is logging the data type of the variable `miEntero` to the console.
+The `typeof` operator in JavaScript is used to determine the type of a variable or value. In this
+case, it will output `number` to the console, indicating that the variable `miEntero` is of type
+number. */
+console.log(typeof miEntero);
+
+/* The code `let miFlotatante = 7.0; console.log(miFlotatante);` is declaring a variable `miFlotatante`
+and assigning it the floating-point number value `7.0`. Then it logs the value of `miFlotatante` to
+the console, which will output integer number `7`. */
+let miFlotatante = 7.0;
+console.log(miFlotatante);
+
+/* `console.log(typeof miFlotatante);` is logging the data type of the variable `miFlotatante` to the
+console. The `typeof` operator in JavaScript is used to determine the type of a variable or value.
+In this case, it will output `number` to the console, indicating that the variable `miFlotatante` is
+of type number. */
+console.log(typeof miFlotatante);
+
+/* The code `let miCadena = 'Hola'; console.log(miCadena);` is declaring a variable `miCadena` and
+assigning it the string value `'Hola'`. Then it logs the value of `miCadena` to the console, which
+will output `'Hola'`. */
+let miCadena = 'Hola';
+console.log(miCadena);
+
+/* `console.log(typeof miCadena);` is logging the data type of the variable `miCadena` to the console.
+The `typeof` operator in JavaScript is used to determine the type of a variable or value. In this
+case, it will output `string` to the console, indicating that the variable `miCadena` is of type
+string. */
+console.log(typeof miCadena);
+
+/* The code `let miBoolean = true; console.log(miBoolean);` is declaring a variable `miBoolean` and
+assigning it the boolean value `true`. Then it logs the value of `miBoolean` to the console, which
+will output `true`. */
+let miBoolean = true;
+console.log(miBoolean);
+
+/* `console.log(typeof miBoolean);` is logging the data type of the variable `miBoolean` to the
+console. The `typeof` operator in JavaScript is used to determine the type of a variable or value.
+In this case, it will output `boolean` to the console, indicating that the variable `miBoolean` is
+of type boolean. */
+console.log(typeof miBoolean);
+
+/* The code `let miNull = null; console.log(miNull);` is declaring a variable `miNull` and assigning it
+the value `null`, which represents the intentional absence of any object value. Then it logs the
+value of `miNull` to the console, which will output `null`. */
+let miNull = null;
+console.log(miNull);
+
+/* `console.log(typeof miNull);` is logging the data type of the variable `miNull` to the console. The
+`typeof` operator in JavaScript is used to determine the type of a variable or value. In this case,
+it will output `object` to the console, indicating that the variable `miNull` is of type object. */
+console.log(typeof miNull);
+
+/* The code `let miUndefined = undefined; console.log(miUndefined);` is declaring a variable
+`miUndefined` and assigning it the value `undefined`. Then it logs the value of `miUndefined` to the
+console, which will output `undefined`. This is a common practice to initialize a variable without
+assigning a specific value to it, indicating that it has not been defined yet or does not have a
+meaningful value at that point in the code execution. */
+let miUndefined = undefined;
+console.log(miUndefined);
+
+/* `console.log(typeof miUndefined);` is logging the data type of the variable `miUndefined` to the
+console. The `typeof` operator in JavaScript is used to determine the type of a variable or value.
+In this case, it will output `undefined` to the console, indicating that the variable `miUndefined`
+is of type undefined. */
+console.log(typeof miUndefined);
