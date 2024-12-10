2a77d7fb591f62c27fa9854d9cdc895cb6fbe090
Carlos Cruz Rangel <carlosrangel.tech@gmail.com>
2024-12-09 19:12:45 -0600

    Create 1-manejo-de-variables.js

---
+++ ManejoDeVariables/1-manejo-de-variables.js
@@ -0,0 +1,25 @@
+/* This JavaScript code snippet is declaring two variables `miNumero1` and `miNumero2` and assigning
+values to them. */
+
+/* The code `var miNumero1 = 15;` is declaring a variable named `miNumero1` using the `var` keyword and
+assigning it a value of `15`. This variable is then logged to the console using
+`console.log(miNumero1);`, which will output `15` to the console when the code is executed. */
+var miNumero1 = 15;
+console.log(miNumero1);
+
+/* The code `let miNumero2 = 20;` is declaring a variable named `miNumero2` using the `let` keyword and
+assigning it a value of `20`. */
+let miNumero2 = 20 ;
+console.log(miNumero2);
+
+/* The code `let miNumero3 = 30; console.log(miNumero2);` is declaring a variable named `miNumero3`
+using the `let` keyword and assigning it a value of `30`. After that, it is logging the value of the
+variable `miNumero2` to the console. */
+let miNumero3 = 30 ;
+console.log(miNumero2);
+
+/* The code `let  = 10;` is declaring a variable named `` using the `let` keyword
+and assigning it a value of `10`. This variable is then logged to the console using
+`console.log();`, which will output `10` to the console when the code is executed. */
+let $miNumero1= 10;
+console.log($miNumero1);
