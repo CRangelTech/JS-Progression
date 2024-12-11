94b4333fd98b5a17f173582e4bb406aeae776091
Carlos Cruz Rangel <carlosrangel.tech@gmail.com>
2024-12-10 21:49:36 -0600

    Create 5-constantes.js

---
+++ ManejoDeVariables/5-constantes.js
@@ -0,0 +1,19 @@
+//Constantes
+
+/* The code is declaring a constant variable `MI_CONSTANTE` with an initial value of 10. However, the
+line `//MI_CONSTANTE = 20;` is a comment and not an active code line, so it does not change the
+value of `MI_CONSTANTE`. Finally, the code logs the value of `MI_CONSTANTE` to the console, which
+will output `10`. */
+const MI_CONSTANTE = 10;
+//MI_CONSTANTE = 20;
+console.log(MI_CONSTANTE);
+
+/* The line `console.log(Math.PI);` is logging the value of the mathematical constant PI to the
+console. The value of `Math.PI` is approximately 3.14159. */
+console.log(Math.PI);
+
+/* The code is declaring a constant variable `SEGUNDOS_POR_MINUTO` with an initial value of 60. Then,
+it logs the value of `SEGUNDOS_POR_MINUTO` to the console, which will output `60`. This constant
+represents the number of seconds in a minute. */
+const SEGUNDOS_POR_MINUTO = 60;
+console.log(SEGUNDOS_POR_MINUTO);
